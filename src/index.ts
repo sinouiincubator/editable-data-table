a8477d70e3fe06c3deb9b25c5367dab5969e0f08
tianyanqiu <tianyanqiu@sinosoft.com.cn>
2019-07-09 22:35:03 +0800

    chore:0.1.1

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,14 @@ import EditableDataTable, {
 } from './EditableDataTable';
 import TableColumn, { Props as TableColumnProps } from './TableColumn';
 import EditorProps from './shared/EditorProps';
+import useEditingList from './useEditingList';
 
 export default EditableDataTable;
 
-export { EditableDataTableProps, TableColumn, TableColumnProps, EditorProps };
+export {
+  EditableDataTableProps,
+  TableColumn,
+  TableColumnProps,
+  EditorProps,
+  useEditingList,
+};
